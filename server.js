fc08e58f24e9a1920b7c134bf159485b94fa2cad
Pratyoos Panta <pratyoos.panta@outlook.com>
2025-07-05 14:09:24 +0545

    initial commit

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
   res.send('CourseHub API is running...');
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
